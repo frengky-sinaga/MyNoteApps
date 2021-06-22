2e76873d49c9ff870c78ba5c1ca07fac03d631b9
Frengky <fsinaga9c@gmail.com>
2021-06-22 15:42:59 +0700

    add viewModelFactory, dateHelper, NoteDiffCallback

---
+++ app/src/main/java/com/learning/mynoteapps/ViewModelFactory.kt
@@ -0,0 +1,33 @@
+package com.learning.mynoteapps
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import com.learning.mynoteapps.ui.insert.NoteAddUpdateViewModel
+import com.learning.mynoteapps.ui.main.MainViewModel
+import java.lang.IllegalArgumentException
+
+class ViewModelFactory private constructor(private val mApplication: Application) {
+    companion object {
+        @Volatile
+        private var INSTANCE: ViewModelFactory? = null
+
+        @JvmStatic
+        fun getInstance(application: Application): ViewModelFactory {
+            if (INSTANCE == null) {
+                synchronized(ViewModelFactory::class.java) {
+                    INSTANCE = ViewModelFactory(application)
+                }
+            }
+            return INSTANCE as ViewModelFactory
+        }
+    }
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(mApplication) as T
+        } else if (modelClass.isAssignableFrom(NoteAddUpdateViewModel::class.java)) {
+            return NoteAddUpdateViewModel(mApplication) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
+}
---
+++ app/src/main/java/com/learning/mynoteapps/helper/NoteDiffCallback.kt
@@ -0,0 +1,20 @@
+package com.learning.mynoteapps.helper
+
+import androidx.recyclerview.widget.DiffUtil
+import com.learning.mynoteapps.db.Note
+
+class NoteDiffCallback(private val mOldNoteList: List<Note>, private val mNewNoteList: List<Note>) :
+    DiffUtil.Callback() {
+    override fun getOldListSize(): Int = mOldNoteList.size
+
+    override fun getNewListSize(): Int = mNewNoteList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        mOldNoteList[oldItemPosition].id == mNewNoteList[newItemPosition].id
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldEmployee = mOldNoteList[oldItemPosition]
+        val newEmployee = mNewNoteList[newItemPosition]
+        return oldEmployee.title == newEmployee.title && oldEmployee.description == newEmployee.description
+    }
+}
